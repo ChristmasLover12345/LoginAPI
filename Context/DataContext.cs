67b8c612182dbea93a38de86b08b97ffc9823a32
ChristmasLover12345 <madnes0205@gmail.com>
2025-03-11 15:58:29 -0700

    some progress, trying to connect to the database

---
+++ Context/DataContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoginAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginAPI.Context
+{
+    public class DataContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
+using LoginAPI.Context;
+using LoginAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<UserServices>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>{
+    options.AddPolicy("AllowAll",
+        policy=>{
+            policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
+var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
