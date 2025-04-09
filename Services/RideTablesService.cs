009608ef4ccd4373a71f45bb5be4c94c6480a1d0
ChristmasLover12345 <madnes0205@gmail.com>
2025-04-09 12:06:43 -0700

    changes

--- Services/RideTablesService.cs
+++ Services/RideTablesService.cs
@@ -173,7 +173,7 @@ namespace LoginAPI.Services
             {
 
                 newProfile.UserName = profile.UserName;
-                newProfile.userId = profile.userId;
+                newProfile.UserId = profile.UserId;
                 newProfile.Name = profile.Name;
                 newProfile.Location = profile.Location;
                 newProfile.BikeType = profile.BikeType;
