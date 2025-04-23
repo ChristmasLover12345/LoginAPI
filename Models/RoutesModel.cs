8f017b48c5f1f095ebcc6a591ed3c45cbdfdd7d5
Chottcodes <cwellshott@gmail.com>
2025-04-23 11:06:11 -0700

    spaces

--- Models/RoutesModel.cs
+++ Models/RoutesModel.cs
@@ -11,22 +11,22 @@ namespace LoginAPI.Models
     public class RoutesModel
     {
 
-   
+
         public int Id { get; set; }
         public int CreatorId { get; set; }
 
 
         public string? RouteName { get; set; }
         public string? RouteDescription { get; set; }
-        public string? ImageUrl { get; set;}
+        public string? ImageUrl { get; set; }
 
         public string? CityName { get; set; }
-        
+
         public DateTime DateCreated { get; set; }
         public bool IsPrivate { get; set; }
         public bool IsDeleted { get; set; }
 
-         virtual public List<CoordinatesModel>? PathCoordinates { get; set;}
+        virtual public List<CoordinatesModel>? PathCoordinates { get; set; }
         public List<LikesModel>? Likes { get; set; }
         public List<CommentsModel>? Comments { get; set; }
 
