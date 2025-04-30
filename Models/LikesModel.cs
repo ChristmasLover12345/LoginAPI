36c7fc5549eecc244c791be5eeec28881f41ed77
Chottcodes <cwellshott@gmail.com>
2025-04-30 14:44:10 -0700

    space

--- Models/LikesModel.cs
+++ Models/LikesModel.cs
@@ -10,7 +10,6 @@ namespace LoginAPI.Models
         public int Id { get; set; }
         public int UserId { get; set; }
         public int? PostId { get; set; }
-
         public bool IsDeleted { get; set; }
         public DateTime CreatedAt { get; set; }
     }
